21bc2f30e89a30582e29980d8b556b2a3e2d79e7
league <league@imac-18.attlocal.net>
2022-12-08 19:49:41 -0800

    working on crazy laady

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,17 +2,61 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+import java.awt.AWTException;
 import java.awt.Color;
 
+
 public class RobotColorChooser {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AWTException {
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot rob = new Robot();
+		
+		
+		int count = 0;
+	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
+		
+		while(count < 25) {
+		
+		String color = JOptionPane.showInputDialog("What color should Rob draw with?");
+		
+		
+		if(color.equals("blue")) {
+			rob.setPenColor(99,125, 186);
+		}
+		else if(color.equals("green")) {
+			rob.setPenColor(110, 199, 88);
+		}
+		else {
+			rob.setRandomPenColor();
+		}
+		
+		rob.setSpeed(20);
+	    //2. Make the robot draw a shape (this will take more than one line of code)
+		rob.penDown();
+		rob.turn(90);
+		rob.move(250);
+		rob.turn(90);
+		rob.move(250);
+		rob.turn(90);
+		rob.move(250);
+		rob.turn(90);
+		rob.move(250);
 		//3. Set the pen width to 10
+		rob.setPenWidth(10);
+		
+		count += 1;
+		}
 		//4. Ask the user what color pen they would like the robot to draw with
+		
 		//5. Use an if/else statement to set the pen color that the user requested
+		
         //6. If the user doesn't enter anything, choose a random color
+		
+		}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 
@@ -21,4 +65,4 @@ public class RobotColorChooser {
 
 
 	}
-}
+
